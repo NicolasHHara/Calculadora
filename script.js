6b2bd15f4e39cc2e25f2b3e8a6a01dcd35112220
Nicolas <nico.hhara@gmail.com>
2025-03-30 13:25:51 -0300

    Só falta o ponto

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function insert(num)
+{
+    var numero = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = numero + num;
+}
+function clean() 
+{
+    document.getElementById('result').innerHTML = ""; 
+}
+function calcular() 
+{
+    var resultado = document.getElementById('result').innerHTML; 
+    if (resultado) {
+        try {
+            document.getElementById('result').innerHTML = eval(resultado);
+        } catch (e) {
+            alert("Error");
+            clean ();
+        }
+    } else {
+        document.getElementById('result').innerHTML = "NaN";
+    }
+}
